60c823920fceaf0c81456db7c8382b02de35270b
xiaohaoyong <xiaohaoyong@126.com>
2017-05-20 11:38:19 +0800

    first

---
+++ data/config/database.ini.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('IN_IAEWEB')) exit();
+return array (
+  'host' => 'jzfupin.cn',
+  'username' => 'jzfupin',
+  'password' => 'FpK2nvnHGmRH6qcV',
+  'dbname' => 'jzfupin',
+  'prefix' => 'io_',
+  'port' => '3306',
+  'charset' => 'utf8',
+);
